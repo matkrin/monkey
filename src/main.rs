1e1dd5d3e4308c5446d6ff344c46950519a692bf
matkrin <matkrin@protonmail.com>
2024-09-14 17:38:07 +0200

    REPL

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,39 @@
+use std::io;
+use std::io::Write;
+
 use lexer::Lexer;
+use token::Token;
 
 mod ast;
 mod lexer;
 mod parser;
 mod token;
 
+const PROMPT: &str = "monkey❯";
+
+
+
 fn main() {
-    let lexer = Lexer::new("");
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
+    let mut input = String::new();
+
+    'outer: loop {
+        input.clear();
+        write!(stdout, "{} ", PROMPT).expect("Failed writing to stdout");
+        io::stdout().flush().expect("Failed to flush stdout");
+
+        stdin
+            .read_line(&mut input)
+            .expect("Failed to read line from stdin");
+
+        let mut lexer = Lexer::new(&input);
+
+        loop {
+            match lexer.next_token() {
+                Token::Eof => continue 'outer,
+                t => writeln!(stdout, "{}", t).expect("Failed to write to stdout"),
+            }
+        }
+    }
 }
--- src/token.rs
+++ src/token.rs
@@ -1,3 +1,5 @@
+use core::fmt;
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum Token {
     Illegal,
@@ -48,6 +50,40 @@ impl Token {
             }
         } else {
             self
+        }
+    }
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self {
+            Token::Illegal => write!(f, "Illegal"),
+            Token::Eof => write!(f, "Eof"),
+            Token::Ident(x) => write!(f, "Ident: {}", x),
+            Token::Int(x) => write!(f, "Int {}", x),
+            Token::Assign => write!(f, "="),
+            Token::Plus => write!(f, "+", ),
+            Token::Minus => write!(f, "-"),
+            Token::Bang => write!(f, "!"),
+            Token::Asterisk => write!(f, "*"),
+            Token::Slash => write!(f, "/"),
+            Token::LessThan => write!(f, "<"),
+            Token::GreaterThan => write!(f, ">"),
+            Token::Equal => write!(f, "=="),
+            Token::NotEqual => write!(f, "!="),
+            Token::Comma => write!(f, ","),
+            Token::Semicolon => write!(f, ";"),
+            Token::LParen => write!(f, "("),
+            Token::RParen => write!(f, ")"),
+            Token::LBrace => write!(f, "{{"),
+            Token::RBrace => write!(f, "}}"),
+            Token::Function => write!(f, "fn"),
+            Token::Let => write!(f, "let"),
+            Token::True => write!(f, "true"),
+            Token::False => write!(f, "false"),
+            Token::If => write!(f, "if"),
+            Token::Else => write!(f, "else"),
+            Token::Return => write!(f, "return"),
         }
     }
 }
