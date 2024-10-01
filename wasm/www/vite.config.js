0cc10a19a11273ec646daabcae4ae1c8884f7929
matkrin <matkrin@protonmail.com>
2024-10-01 20:21:13 +0200

    Deploy wasm repl in xterm-js-rs to github pages

--- wasm/www/vite.config.js
+++ wasm/www/vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm"
 
 export default defineConfig({
+    base: "/monkey/",
     plugins: [
         wasm(),
-    ]
+    ],
+    build: {
+        target: "esnext",
+    },
 })
